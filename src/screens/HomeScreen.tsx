a12121ca826b3464104acc3d5ef5af5ef4c31862
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-10-14 12:40:42 -0400

    feat: add navbar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,22 +8,35 @@ import MoviesCarrousel from '../components/MoviesCarrousel/MoviesCarrousel';
 import { Movie } from '../services/domain/Movie';
 import { TMDBRepository } from '../services/infrastructure/TMDBRepository';
 import { GetPopularMovies } from '../services/application/GetPopularMovies';
+import { GetGenres } from '../services/application/GetGenres';
+import { Genre } from '../services/domain/Genre';
+import Navbar from '../components/Navbar/Navbar';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 const movieRepository = new TMDBRepository();
 const getPopularMovies = new GetPopularMovies(movieRepository);
+const getGenres = new GetGenres(movieRepository);
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+  const [genres, setGenres] = useState<Genre[]>([]);
+
+  const [activeGenreId, setActiveGenreId] = useState<number>(0);
 
   const fetchMovies = async () => {
     const data = await getPopularMovies.execute(1);
     setPopularMovies(data.slice(0, 5));
   };
 
+  const fetchGenres = async () => {
+    const data = await getGenres.execute();
+    setGenres(data);
+  };
+
   useEffect(() => {
     fetchMovies();
+    fetchGenres();
   }, []);
 
   return (
@@ -34,13 +47,15 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
           contentContainerStyle={{ paddingBottom: 24 }}
           nestedScrollEnabled
         >
-          <MoviesCarrousel
-            popularMovies={popularMovies}
+          <Navbar
+            genres={[{ id: 0, name: 'All' }, ...genres]}
+            activeId={activeGenreId}
+            onSelect={setActiveGenreId}
           />
+          <MoviesCarrousel popularMovies={popularMovies} />
         </ScrollView>
       </SafeAreaProvider>
     </GestureHandlerRootView>
-
   );
 };
 
