7eb8352dc419cc0ef2ba575350b457f4bd04ce37
Mariana Vallejos <mariana.vallejos@assuresoft.tech>
2025-10-17 07:22:26 -0700

    feat: add navigation from movie item to movie detail screen

--- src/components/molecules/movieItem/MovieItem.tsx
+++ src/components/molecules/movieItem/MovieItem.tsx
@@ -4,15 +4,22 @@ import { Movie } from '../../../services/domain/Movie'
 import { MovieItemStyles } from './MovieItem.styles'
 import { CustomText } from '../../atoms/CustomText/CustomText'
 import MovieBanner from '../../atoms/MovieBanner/MovieBanner'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../../../navigation/types'
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
 type MovieItemProps = {
     item: Movie,
 }
 
 const MovieItem = ({item} : MovieItemProps) => {
+    const navigation = useNavigation<HomeScreenNavigationProp>()
+
     return (
-        <TouchableOpacity style={MovieItemStyles.card} onPress={() => console.log(item.originalTitle)}>
-            <MovieBanner movie={item} width={120} customStyle={MovieItemStyles.poster} posterImg/>
+        <TouchableOpacity style={MovieItemStyles.card} onPress={() => navigation.navigate('Details', {itemId: item.id})}>
+            <MovieBanner movie={item} width={120} customStyle={MovieItemStyles.poster}/>
             <CustomText numberOfLines={1}>{item.originalTitle}</CustomText>
         </TouchableOpacity>
     )
